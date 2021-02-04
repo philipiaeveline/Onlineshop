b2ec61511c204a1936ff61496b9182a1227b84ec
philipiaeveline <philipiaeveline13@gmail.com>
2021-02-04 20:08:46 +0300

    add

--- ShopClone/settings.py
+++ ShopClone/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import cloudinary
-import django_heroku
-import dj_database_url
-from decouple import config,Csv
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# import cloudinary
+# import django_heroku
+# import dj_database_url
+# from decouple import config,Csv
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
