ab1df2deafa37dd65759bc1fadad957f5cecf204
philipiaeveline <philipiaeveline13@gmail.com>
2021-02-04 20:24:14 +0300

    add

--- ShopClone/urls.py
+++ ShopClone/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('OnlineShop.urls'))
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('OnlineShop.urls'))
 
-]
+# ]
 
